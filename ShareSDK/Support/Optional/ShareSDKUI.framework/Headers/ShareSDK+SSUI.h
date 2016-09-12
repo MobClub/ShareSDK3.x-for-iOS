47240dc9d6888db76cd4c1eee77ec4b9ed317671
JinghuangLiu <liujinghuang@liujhs-Mac.local>
2016-09-12 10:03:30 +0800

    v3.4.2
    1、适配iOS10。
    2、ShareSDKUI模块ShareSDK+SSUI新增2个接口，用于支持使用UI下进行配置文件分享。
    3、修复印象笔记分享含链接的文本可能失败的问题。
    4、修复VKontakte分享图片可能失败的问题。
    5、新浪微博在iOS10下分享授权需要额外配置NSExceptionMinimumTLSVersion。
    6、QQ空间在iOS10下分享需要额外配置LSApplicationQueriesSchemes。
    以下用户建议更新：
    1、需要适配iOS10的用户。
    2、需要使用ShareSDK提供的分享菜单栏、分享编辑页面，且使用配置文件方式分享的用户。
    3、印象笔记分享文本中含有链接地址的用户。
    4、使用VKontakte分享图片的用户。

--- ShareSDK/Support/Optional/ShareSDKUI.framework/Headers/ShareSDK+SSUI.h
+++ ShareSDK/Support/Optional/ShareSDKUI.framework/Headers/ShareSDK+SSUI.h
@@ -60,6 +60,40 @@ typedef void (^SSUIShareStateChangedHandler) (SSDKResponseState state,
                                       onShareStateChanged:(SSUIShareStateChangedHandler)shareStateChangedHandler;
 
 /**
+ *  根据配置文件显示分享菜单
+ *
+ *  @param view                     要显示菜单的视图, iPad版中此参数作为弹出菜单的参照视图
+ *  @param items                    菜单项，如果传入nil，则显示已集成的平台列表
+ *  @param contentName              ShareContent.xml内<Content>标签name属性的值
+ *  @param customFields             自定义字段
+ *  @param shareStateChangedHandler 分享状态变更事件
+ *
+ *  @return 分享菜单控制器
+ */
++ (SSUIShareActionSheetController *)showShareActionSheet:(UIView *)view
+                                                   items:(NSArray *)items
+                                             contentName:(NSString *)contentName
+                                            customFields:(NSDictionary *)customFields
+                                     onShareStateChanged:(SSUIShareStateChangedHandler)shareStateChangedHandler;
+
+/**
+ *  根据配置文件显示内容编辑视图
+ *
+ *  @param platformType             分享的平台类型
+ *  @param otherPlatformTypes       除分享平台外，还可以分享的平台类型。
+ *  @param contentName              ShareContent.xml内<Content>标签name属性的值
+ *  @param customFields             自定义字段
+ *  @param shareStateChangedHandler 分享状态变更事件
+ *
+ *  @return 内容编辑视图控制器
+ */
++ (SSUIShareContentEditorViewController *)showShareEditor:(SSDKPlatformType)platformType
+                                       otherPlatformTypes:(NSArray *)otherPlatformTypes
+                                              contentName:(NSString *)contentName
+                                             customFields:(NSDictionary *)customFields
+                                      onShareStateChanged:(SSUIShareStateChangedHandler)shareStateChangedHandler;
+
+/**
  *  设置支持的页面方向
  */
 + (void)setSupportedInterfaceOrientation:(UIInterfaceOrientationMask)toInterfaceOrientation;
